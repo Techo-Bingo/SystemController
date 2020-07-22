e4c3a4f110124f2aa4abe20be6c201b233a32a31
Bingo <linux_bingo@163.com>
2020-07-22 18:00:56 +0800

    Update common_function.sh

--- cmds/package/common_function.sh
+++ cmds/package/common_function.sh
@@ -30,7 +30,8 @@ function compress()
 {
 	cd ${g_task_dir}
 	local dst_file=$1
-	which 7za &>/dev/null && { echo "${dst_file}.7z"; 7za a ${dst_file}.7z * &>/dev/null && { chmod 777 ${dst_file}.7z; return 0; } || return 1; }
+	# 7za 速度太慢了
+	# which 7za &>/dev/null && { echo "${dst_file}.7z"; 7za a ${dst_file}.7z * &>/dev/null && { chmod 777 ${dst_file}.7z; return 0; } || return 1; }
 	which tar &>/dev/null && { echo "${dst_file}.tar.gz"; tar zcvf ${dst_file}.tar.gz * &>/dev/null && { chmod 777 ${dst_file}.tar.gz; return 0; } || return 1; }
 	which zip &>/dev/null && { echo "${dst_file}.zip"; zip ${dst_file}.zip * &>/dev/null && { chmod 777 ${dst_file}.zip; return 0; } || return 1; }
 	return 1
