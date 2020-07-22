99fdae1d54cbf2600821499e1e74749cd4a741f2
Bingo <linux_bingo@163.com>
2020-07-22 18:13:17 +0800

    Update my_ssh.py

--- my_ssh.py
+++ my_ssh.py
@@ -26,10 +26,7 @@ class SSH(object):
             if not self._ssh_client:
                 self._ssh_client = paramiko.SSHClient()
                 self._ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                self._ssh_client.connect(self._host_ip,
-                                         username=self._user_name,
-                                         password=self._user_pwd
-                                         )
+                self._ssh_client.connect(self._host_ip, username=self._user_name, password=self._user_pwd)
             return self._ssh_client.exec_command(cmd)
         except Exception as e:
             raise SSHError(e)
@@ -71,24 +68,23 @@ class SSHUtil:
             info = ssh_inst.execute(cmd, root)[1].read().strip()
         except Exception as e:
             return None, e
-        else:
+        try:
             return str(info, encoding='utf-8'), None
+        except:
+            return str(info, encoding='GBK'), None
 
     @classmethod
     def exec_ret(cls, ssh_inst, cmd, root=False):
         try:
             info = ssh_inst.execute(cmd, root)[1]
+            return info.channel.recv_exit_status(), None
         except SSHError as e:
             return -1, e
-        else:
-            return info.channel.recv_exit_status(), None
 
     @classmethod
     def user_login(cls, ssh_inst, user_name):
         root = True if user_name == 'root' else False
-        ret, err = cls.exec_ret(ssh_inst,
-                                'whoami|grep %s' % user_name,
-                                root)
+        ret, err = cls.exec_ret(ssh_inst, 'whoami|grep %s' % user_name, root)
         if ret:
             return False, err
         else:
@@ -98,9 +94,7 @@ class SSHUtil:
     def upload_file(cls, ssh_inst, local, remote):
         path = os.path.split(remote)[0]
         try:
-            ssh_inst.execute('mkdir -p {0};chmod 777 {0}'.format(path),
-                             root=True
-                             )
+            ssh_inst.execute('mkdir -p {0};chmod 777 {0}'.format(path), root=True)
             # 此处等待一小段时间，否则后面upload会失败
             sleep(0.2)
             ssh_inst.upload(local, remote)
