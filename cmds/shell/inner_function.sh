0a57de0255e3c7eeb4e7b4878a9a9d19e0b5b55b
Bingo <linux_bingo@163.com>
2020-10-14 18:36:30 +0800

    Update inner_function.sh

--- cmds/shell/inner_function.sh
+++ cmds/shell/inner_function.sh
@@ -20,12 +20,13 @@ function sync_call_shell()
 {
     local task=$1
     local shell=$2
-    local params=$3
+    shift 2
+    local params=$*
     make_task_dir ${task}
     cd ${g_task_dir}
     dos2unix ${g_home_dir}/${shell}
     echo "sh ${g_home_dir}/${shell} ${params}" >${g_task_dir}/__call__.txt
-    sh ${g_home_dir}/${shell} "${params}" >${g_task_dir}/__print__.txt 2>&1
+    sh ${g_home_dir}/${shell} ${params} >${g_task_dir}/__print__.txt 2>&1
     return $?
 }
 
@@ -33,12 +34,13 @@ function async_call_shell()
 {
     local task=$1
     local shell=$2
-    local params=$3
+    shift 2
+    local params=$*
     make_task_dir ${task}
     cd ${g_task_dir}
     dos2unix ${g_home_dir}/${shell}
     echo "sh ${g_home_dir}/${shell} ${params}" >${g_task_dir}/__call__.txt
-    sh ${g_home_dir}/${shell} "${params}" >${g_task_dir}/__print__.txt 2>&1 &
+    sh ${g_home_dir}/${shell} ${params} >${g_task_dir}/__print__.txt 2>&1 &
 }
 
 # 有些环境登陆后可能会预先打印用户环境信息，获取打印信息会造成混乱，
@@ -64,29 +66,6 @@ function kill_shell()
     local shell=$1
     local top_pid=$(ps -ef|grep -w "${shell}"|awk '{print $2}'|head -1)
     pstree ${top_pid} -p|tr '(' '\n'|grep '^[0-9]'|cut -d')' -f1|xargs kill -9
-}
-
-function upload_prev_check()
-{
-    local dest_dir=$1
-    local upload_dir=$2
-    [ -d "${dest_dir}" ] || return 1
-    mkdir ${upload_dir}
-    chmod 777 ${upload_dir}
-    return 0
-}
-
-function move_file()
-{
-    local src_file=$1
-    local dest_file=$2
-    local mod_attr=$3
-    local own_attr=$4
-    [ -f ${src_file} ] || return 1
-    mv -f ${src_file} ${dest_file} || return 2
-    chmod ${mod_attr} ${dest_file} || return 3
-    chown ${own_attr} ${dest_file} || return 4
-    return 0
 }
 
 $*
