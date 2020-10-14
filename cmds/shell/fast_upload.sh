e5ac5a025f29dd5a6eee27d83084986323c6c5a3
Bingo <linux_bingo@163.com>
2020-10-14 18:35:36 +0800

    Create fast_upload.sh

---
+++ cmds/shell/fast_upload.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+source $(dirname $0)/common_function.sh
+
+SRC_FILE=$1
+DEST_PATH=$2
+MOD_ATTR=$3
+OWN_ATTR=$4
+
+function upload()
+{
+	[ -z "${SRC_FILE}" -o -z "${DEST_PATH}" -o -z "${MOD_ATTR}" -o -z "${OWN_ATTR}" ] && report_err "40" "invaild args"
+	
+	[ -f "${SRC_FILE}" ] || report_err "40" "${SRC_FILE} No such file"
+	
+	[ -d "${DEST_PATH}" ] || report_err "40" "${DEST_PATH} No such directory"
+
+	cd ${g_task_dir}
+	local dest_file="${DEST_PATH}/$(basename ${SRC_FILE})"
+	
+	mv -f ${SRC_FILE} ${dest_file} || report_err "50" "move file to ${DEST_PATH} failed !"
+	chmod ${MOD_ATTR} ${dest_file} || report_err "60" "chmod ${MOD_ATTR} ${dest_file} failed !"
+	chown ${OWN_ATTR} ${dest_file} || report_err "70" "chown ${OWN_ATTR} ${dest_file} failed !"
+	
+	chmod -R 777 ${g_task_dir}/*
+	report_info "100" "NULL"
+}
+
+function main()
+{
+    init
+    upload
+}
+
+main &>/dev/null
