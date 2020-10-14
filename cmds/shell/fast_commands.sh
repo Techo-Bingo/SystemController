5fbd29716728cff282466584693d8f210e74998a
Bingo <linux_bingo@163.com>
2020-10-14 18:34:14 +0800

    Update fast_commands.sh

--- cmds/shell/fast_commands.sh
+++ cmds/shell/fast_commands.sh
@@ -1,1 +1,17 @@
-df -h
+#!/bin/bash
+source $(dirname $0)/common_function.sh
+
+SHELL_PATH=$1
+
+function main()
+{
+    init
+    chmod +x ${SHELL_PATH}
+    sh ${SHELL_PATH}
+
+    chmod -R 777 ${g_task_dir}/*
+	  report_info "100" "NULL"
+}
+
+main 
+
