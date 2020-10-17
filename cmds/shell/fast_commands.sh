5384933754a5490aa03ec28b56437368e12833d1
Bingo <linux_bingo@163.com>
2020-10-17 13:28:32 +0800

    Update fast_commands.sh

--- cmds/shell/fast_commands.sh
+++ cmds/shell/fast_commands.sh
@@ -6,10 +6,12 @@ SHELL_PATH=$1
 function main()
 {
     init
-    chmod +x ${SHELL_PATH}
+    dos2unix ${SHELL_PATH} 2>/dev/null
+    chmod +x ${SHELL_PATH} 2>/dev/null
     sh ${SHELL_PATH}
 
-    chmod -R 777 ${g_task_dir}/*
+    chmod -R 777 ${g_task_dir}/* 2>/dev/null
+    rm -f ${SHELL_PATH} 2>/dev/null
     report_info "100" "NULL"
 }
 
