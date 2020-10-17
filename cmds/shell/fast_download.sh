f281f9e6c0f650c88113b32bff7b9186705d96a3
Bingo <linux_bingo@163.com>
2020-10-17 13:28:55 +0800

    Update fast_download.sh

--- cmds/shell/fast_download.sh
+++ cmds/shell/fast_download.sh
@@ -16,12 +16,12 @@ function download()
 	if [ -f "${FILE_PATH}" ]
 	then
 		report_info "30" "Copying file..."
-		cp -f ${FILE_PATH} ./${pack_name} 2>__error__
-		[ $? -ne 0 ] && report_err '60' "$(head -1 __error__)"
+		cp -f ${FILE_PATH} ./${pack_name} 2>${g_error}
+		[ $? -ne 0 ] && report_err '60' "$(head -1 ${g_error})"
 	else
 		report_info "30" "Copying directory..."
-		cp -rf ${FILE_PATH} ./ 2>__error__
-		[ $? -ne 0 ] && report_err '60' "$(head -1 __error__)"
+		cp -rf ${FILE_PATH} ./ 2>${g_error}
+		[ $? -ne 0 ] && report_err '60' "$(head -1 ${g_error})"
 		report_info "90" "Compress start"
 		local pack_name=$(compress ${pack_name})
 		[ $? -ne 0 ] && report_err '95' "Compress ${pack_name} failed"
