b606ec1dac7aa2e1e621ae3e4649420784f4abf1
Bingo <linux_bingo@163.com>
2021-08-04 16:30:30 +0800

    Update refresh_resource.sh

--- Interface/Scripts/refresh_resource.sh
+++ Interface/Scripts/refresh_resource.sh
@@ -5,8 +5,14 @@ mkdir ${DOWNLOAD_DIR} 2>/dev/null
 
 function main()
 {
-    cp -f example/*.csv ${DOWNLOAD_DIR}
-	chmod 777 ${DOWNLOAD_DIR} ${DOWNLOAD_DIR}/*.csv
+    cp -f /home/ubp/logs/tracecollection/*.csv ${DOWNLOAD_DIR}
+    cd ${DOWNLOAD_DIR}
+    for file in *.csv
+    do
+      local new_name="$(echo $file|awk -F. '{print $1}'|sed 's/_info//').csv"
+      mv -f $file $new_name
+    done
+    chmod 777 ${DOWNLOAD_DIR} ${DOWNLOAD_DIR}/*.csv
 }
 
 main
